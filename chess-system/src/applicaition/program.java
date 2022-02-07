748a3578b8751778dda099a52d511680ab921715
natanaeljn <natanaeljn@outlook.com.br>
2022-02-07 20:25:27 -0300

    chess

--- chess-system/src/applicaition/program.java
+++ chess-system/src/applicaition/program.java
@@ -1,10 +1,14 @@
 package applicaition;
 
+import chess.chessmatch;
+
 public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		chessmatch chessmatch = new chessmatch();
+		UI.printBoard(chessmatch.getPieces());
 	}
 
 }
